3b6ff5e72dae05ebfaee4a612728987c44573a30
Pranav-Saggam <86381364+Pranav-Saggam@users.noreply.github.com>
2021-06-25 02:59:06 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,8 +1,9 @@
+
 import java.io.File;
 import java.io.IOException;
 
     public class Assignment1{
-        public static void findfile(String path,String filename){
+        public static void findfile(String path,String fileextension){
             //Creating a File object for directory
             File directoryPath = new File(path);
             //List of all files and directories
@@ -12,13 +13,13 @@ import java.io.IOException;
                 return;
             }
             for(File file : filesList) {
-                if(file.getName().equals(filename))
+                if(file.getName().endsWith(fileextension))
                 System.out.println("File path: "+file.getAbsolutePath());
 
                 if(file.isDirectory()){
                   /*  System.out.println(file.getAbsolutePath());*/
 
-                    findfile(file.getPath(),filename);
+                    findfile(file.getPath(),fileextension);
                 }
             }
 
@@ -27,10 +28,12 @@ import java.io.IOException;
         }
 
         public static void main(String args[]) throws IOException {
-            String file="Assignment1.java";
-            String path="/home/pranas/IdeaProjects";
-            findfile(path,file);
+            String fileextension=".java";
+            String path="/home/pranas/";
+            findfile(path,fileextension);
 
         }
     }
+
+
 
