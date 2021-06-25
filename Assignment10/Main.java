a73ab50c342b1af244912b9c59eee2f230f7eb02
Pranav-Saggam <86381364+Pranav-Saggam@users.noreply.github.com>
2021-06-25 05:51:12 +0530

    Add files via upload

---
+++ Assignment10/Main.java
@@ -0,0 +1,15 @@
+package Assignment10;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        SList<Integer> sList=new SList<>();
+        SListIterator<Integer> sListIterator=sList.iterator();
+
+        sListIterator.add(1);
+        sListIterator.add(2);
+        
+
+    }
+}
---
+++ Assignment10/Node.java
@@ -0,0 +1,19 @@
+package Assignment10;
+
+class Node<T> {
+    T data;
+    Node<T> next;
+    Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+    Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+    public String toString() {
+        if(data == null) return "Null";
+        return data.toString();
+    }
+}
+
